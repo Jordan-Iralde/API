fff5ecf300858e3e83b39fba800963c3ce400b5d
Jordan Iralde <iraldejordan10@gmail.com>
2026-05-21 05:52:22 -0300

    Update courses.service.ts

--- backend/src/modules/courses/courses.service.ts
+++ backend/src/modules/courses/courses.service.ts
@@ -1,10 +1,13 @@
 import { db } from "../../core/db";
 import { courses } from "../../core/db/schema";
 
+// create-course.dto.ts
+
 type CreateCourseDTO = {
   title: string;
   description: string;
-};
+  appId: number;
+}
 
 export const coursesService = {
   getAll: () => db.select().from(courses),
